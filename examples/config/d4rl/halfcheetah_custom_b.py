991a7fbda3638d1898e9727a784c9fc442c9b684
avecplezir <avecplezir@gmail.com>
2023-09-25 09:01:09 +0400

    upd

---
+++ examples/config/d4rl/halfcheetah_custom_b.py
@@ -0,0 +1,28 @@
+import os
+from .base_mopo import mopo_params, deepcopy
+
+params = deepcopy(mopo_params)
+params.update({
+    'domain': 'HalfCheetah',
+    'task': 'v2',
+    'exp_name': 'halfcheetah_mixed_rt_1',
+    'seed': 4321,
+})
+params['kwargs'].update({
+    'pool_load_path': os.path.expanduser('~/rds/rds-dsk-lab-eWkDxBhxBrQ/dimorl/code/dogo_results/data/MIXED-RT-1.npy'),
+    'bnn_retrain_epochs': 0,
+    'rex_type': 'var',
+    'pool_load_max_size': 101000,
+    'rollout_length': 5,
+    'rollout_batch_size': 50e3,
+    'penalty_coeff': 5.0,
+    'holdout_policy': None,
+    'train_bnn_only': False,
+    'rex': True,
+    'policy_type': 'random_5',
+    'rex_beta': 10.0,
+    'rex_multiply': True,
+    'repeat_dynamics_epochs': 1,
+    'lr_decay': 1.0,
+    'bnn_batch_size': 256
+})
